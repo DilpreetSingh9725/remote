acac3d6a4f29023c26942d31e0359f34a851d1d0
DS10525 <sin17033@sheridancollege.ca>
2025-05-26 17:30:51 -0400

    down1

---
+++ src/fundamentaljava/partimeStudent.java
@@ -0,0 +1,18 @@
+
+package fundamentaljava;
+
+/**
+ *
+ * @author dilpreet singh
+ */
+public class partimeStudent {
+
+    public String getDays() {
+        return days;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+    private String days;
+}
